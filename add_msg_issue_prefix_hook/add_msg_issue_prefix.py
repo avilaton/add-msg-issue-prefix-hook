74bcb8aeeaefbc2ab0311fadd59598f7409d8530
Oriol Vall <oriol.vall.hdez@gmail.com>
2022-03-31 18:26:13 +0200

    Update add_msg_issue_prefix.py

--- add_msg_issue_prefix_hook/add_msg_issue_prefix.py
+++ add_msg_issue_prefix_hook/add_msg_issue_prefix.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
-import sys
 import re
 import subprocess
 
 
 def get_ticket_id_from_branch_name(branch):
-    matches = re.findall('[a-zA-Z]{1,10}-[0-9]{1,5}', branch)
+    matches = re.findall("[a-zA-Z]{1,10}-[0-9]{1,5}", branch)
     if len(matches) > 0:
         return matches[0]
 
@@ -15,17 +14,12 @@ def get_ticket_id_from_branch_name(branch):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("commit_msg_filepath")
-    parser.add_argument(
-        '-t', '--template', default="[{}]",
-        help='Template to render ticket id into',
-    )
     args = parser.parse_args()
     commit_msg_filepath = args.commit_msg_filepath
-    template = args.template
 
     branch = ""
     try:
-        branch = subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"], universal_newlines=True).strip()
+        branch = subprocess.check_output(["git", "symbolic-ref", "--short", "HEAD"], universal_newlines=True).strip()
     except Exception as e:
         print(e)
 
@@ -40,8 +34,10 @@ def main():
         content_subject = content.split("\n", maxsplit=1)[0].strip()
         f.seek(0, 0)
         if issue_number and issue_number not in content_subject:
-            prefix = template.format(issue_number)
-            f.write("{} {}".format(prefix, content))
+            content_list = content.split(":", 1)
+            content_list.insert(1, issue_number)
+
+            f.write("{}({}):{}".format(content_list[0], content_list[1], content_list[2]))
         else:
             f.write(content)
 
