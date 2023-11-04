ccebf284eb6a6b9d2bdeeab5409aa56b86950553
artslob <artslob@yandex.ru>
2023-11-04 15:12:07 +0700

    add test for insert after

--- tests/test_add_msg_issue_prefix.py
+++ tests/test_add_msg_issue_prefix.py
@@ -2,7 +2,7 @@ import add_msg_issue_prefix_hook
 from add_msg_issue_prefix_hook import add_msg_issue_prefix
 import re
 
-def test_modify_commit_message():
+def test_modify_commit_message_simple():
     issue_number = "TASK-1234"
     content = ("\n# Please enter the commit message for your changes."
     "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
@@ -10,5 +10,16 @@ def test_modify_commit_message():
 
     insert_after = re.compile("^")
     result = add_msg_issue_prefix.modify_commit_message(content, issue_number, insert_after)
-    print(result)
+    assert result == expected_result
+
+
+def test_modify_commit_message_with_insert_after():
+    issue_number = "TASK-1234"
+    content = ("task:\n# Please enter the commit message for your changes."
+    "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
+    expected_result = ("task:TASK-1234 \n# Please enter the commit message for your changes."
+    "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
+
+    insert_after = re.compile("^task:")
+    result = add_msg_issue_prefix.modify_commit_message(content, issue_number, insert_after)
     assert result == expected_result
