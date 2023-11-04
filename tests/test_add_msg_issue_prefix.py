9f140bd4f422f785a638605738204e28133c9547
artslob <artslob@yandex.ru>
2023-11-04 15:36:20 +0700

    reformat test

--- tests/test_add_msg_issue_prefix.py
+++ tests/test_add_msg_issue_prefix.py
@@ -1,10 +1,13 @@
 from add_msg_issue_prefix_hook import add_msg_issue_prefix
 import re
 
+
 def test_modify_commit_message_simple():
     issue_number = "TASK-1234"
-    content = ("\n# Please enter the commit message for your changes."
-    "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
+    content = (
+        "\n# Please enter the commit message for your changes."
+        "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n"
+    )
     expected_result = issue_number + " " + content
     insert_after = re.compile(add_msg_issue_prefix.DEFAULT_INSERT_AFTER)
 
@@ -14,10 +17,14 @@ def test_modify_commit_message_simple():
 
 def test_modify_commit_message_with_insert_after():
     issue_number = "TASK-1234"
-    content = ("task:\n# Please enter the commit message for your changes."
-    "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
-    expected_result = ("task:TASK-1234 \n# Please enter the commit message for your changes."
-    "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
+    content = (
+        "task:\n# Please enter the commit message for your changes."
+        "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n"
+    )
+    expected_result = (
+        "task:TASK-1234 \n# Please enter the commit message for your changes."
+        "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n"
+    )
     insert_after = re.compile("^task:")
 
     result = add_msg_issue_prefix.modify_commit_message(content, issue_number, insert_after)
