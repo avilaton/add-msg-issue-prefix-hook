cbe16d51579d38ecf0a6fdadcc4771e3968d6674
artslob <artslob@yandex.ru>
2023-11-04 15:03:41 +0700

    remove strip

--- tests/test_add_msg_issue_prefix.py
+++ tests/test_add_msg_issue_prefix.py
@@ -2,13 +2,13 @@ import add_msg_issue_prefix_hook
 from add_msg_issue_prefix_hook import add_msg_issue_prefix
 import re
 
-def test_q():
+def test_modify_commit_message():
     issue_number = "TASK-1234"
-    content = "# Please enter the commit message for your changes. Lines starting\n# with '#' will be ignored, and an empty message aborts the commit."
-    insert_after = re.compile("^")
+    content = ("\n# Please enter the commit message for your changes."
+    "Lines starting\n# with '#' will be ignored, and an empty message aborts the commit.\n")
+    expected_result = issue_number + " " + content
 
-    print(dir(add_msg_issue_prefix_hook))
-    print(dir(add_msg_issue_prefix))
-    assert 1 == 1
+    insert_after = re.compile("^")
     result = add_msg_issue_prefix.modify_commit_message(content, issue_number, insert_after)
     print(result)
+    assert result == expected_result
